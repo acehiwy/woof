d3e7f819e21744deec2e7ed04d637d7966c4ef64
acehiwy <acehiwy.t@gmail.com>
2022-08-22 06:33:58 +0000

    refactor: add Admonition component to global

--- src/theme/MDXComponents.ts
+++ src/theme/MDXComponents.ts
@@ -3,10 +3,12 @@ import Color from "@site/src/components/Color";
 import MDXComponents from "@theme-original/MDXComponents";
 import TabItem from "@theme/TabItem";
 import Tabs from "@theme/Tabs";
+import Admonition from "@theme/Admonition";
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
+  Admonition,
   Tabs,
   TabItem,
   Color,
